12f34cf56c0226353dae88bbfebc18e4c08746c1
mhchoi09 <rhui21@live.com.ph>
2025-09-17 22:06:14 +0800

    first commit

---
+++ 4GFieldTestTool.Services/FrequencyCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace _4GFieldTestTool.Services
+{
+    public static class FrequencyCalculator
+    {
+        public static decimal DownlinkFrequency(decimal lowestDownlinkFrequency, decimal downlinkEarfcn,
+            decimal downlinkEarfcnOffset)
+        {
+            var downlinkFrequency = lowestDownlinkFrequency
+                                    + (0.1m * (downlinkEarfcn - downlinkEarfcnOffset));
+
+            return downlinkFrequency;
+        }
+
+        public static decimal UplinkFrequency(decimal lowestUplinkFrequency, decimal uplinkEarfcn, decimal uplinkEarfcnOffset)
+        {
+            var uplinkFrequency = lowestUplinkFrequency
+                                    + (0.1m * (uplinkEarfcn - uplinkEarfcnOffset));
+
+            return uplinkFrequency;
+        }
+    }
+}
---
+++ 4GFieldTestTool.Tests/FrequencyCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using _4GFieldTestTool.Services;
+
+namespace _4GFieldTestTool.Tests;
+
+public class FrequencyCalculatorTests
+{
+    [Fact]
+    public void CalculateDownlinkFrequency_WithValidValue_ReturnsCorrectDownlinkFrequency()
+    {
+        var downlinkEarfcn = 400;
+        var downlinkEarfcnOffset = 0;
+        var lowestDownlinkFrequency = 2110;
+
+        var downlinkFrequency = FrequencyCalculator.DownlinkFrequency(lowestDownlinkFrequency, downlinkEarfcn, downlinkEarfcnOffset);
+
+        Assert.Equal(2150m, downlinkFrequency);
+    }
+
+    [Fact]
+    public void CalculateUplinkFrequency_WithValidValue_ReturnsCorrectDownlinkFrequency()
+    {
+        var uplinkEarfcn = 18800;
+        var uplinkEarfcnOffset = 18600;
+        var lowestUplinkFrequency = 1850;
+
+        var uplinkFrequency = FrequencyCalculator.UplinkFrequency(lowestUplinkFrequency, uplinkEarfcn, uplinkEarfcnOffset);
+
+        Assert.Equal(1870m, uplinkFrequency);
+    }
+}
